cb24f2cd7cb5d17c165bdf929e6ece32c7ada497
SujeetYT <sujeet7982422191@gmail.com>
2022-07-25 01:08:47 +0530

    Connected MongoDB Atlas

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-ECHO is on.
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const ObjectId = require("mongodb").ObjectId;
+const url = process.env.URL;
+mongoose.connect(url).then(()=>console.log("Database Connected")).catch((err)=>console.log(err));
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`App running on http://localhost:3000`);
+    console.log(`App running on http://127.0.0.1:3000`);
+});
